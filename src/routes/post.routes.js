5f5e0468d76e68d76593e82d14e9c2d37ce79a91
Amresh <rji72600@gmail.com>
2025-11-30 23:10:49 +0530

    settingUp Gemini

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { authMiddleware } = require('../middlewares/auth.middleware')
+const multer = require('multer')
+
+const upload = multer({storage: multer.memoryStorage()})
 
 router.post('/',
-    authMiddleware)
+    authMiddleware),
+    upload.single("image")
 
 module.exports = router
