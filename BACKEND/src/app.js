a36ec59fdeee219a69ee0ae581e4459dcdfb80eb
Amresh <rji72600@gmail.com>
2026-04-07 15:03:19 +0530

    cors setup

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,26 +1,21 @@
-let express = require("express")
-const cors = require('cors')
-const authRoutes = require('./routes/auth.routes');
-const postRoutes = require('../src/routes/post.routes')
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.routes");
+const postRoutes = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
-const path = require('path')
+const path = require("path");
 
+const app = express();
 
-
-let app = express()
 app.use(express.json());
-app.use(cookieParser())
-app.use(cors({
-    origin: 'http://localhost:5173', // frontend URL
-    credentials: true,               // required for cookies/auth
-  }));
-  app.use(express.static(path.join(__dirname, '../public')))
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true // required for cookies/auth
+  })
+);
 
-app.use('/api/auth',authRoutes)
-app.use('/api/post',postRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
-app.get("*name", (req, res) => {
-    res.sendFile(path.join(__dirname, '../publicindex.html'))
-})
-
-module.exports = app
+module.exports = app;
